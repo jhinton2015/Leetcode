f9d65fb9e31078af3d2b53eb155efc0799d8e79f
jhinton2015 <jhint2011@gmail.com>
2021-10-05 12:04:05 -0400

    Create 1791.py

---
+++ 1791.py
@@ -0,0 +1,6 @@
+## (1791) find the center of a star graph 
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        return edges[0][1]
