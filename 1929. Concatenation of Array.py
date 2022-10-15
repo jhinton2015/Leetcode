f021d562d1da9dc5a1383eb754cd8fc108ffdc3c
jhinton2015 <jhint2011@gmail.com>
2022-10-14 20:51:34 -0400

    Update 1929. Concatenation of Array.py

--- 1929. Concatenation of Array.py
+++ 1929. Concatenation of Array.py
@@ -2,3 +2,4 @@ class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
     #in python you can just add arrays together how the question describes
         return nums + nums
+#new python comment babyy
